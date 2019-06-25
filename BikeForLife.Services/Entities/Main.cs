734fe1bb96a6d3b6da7a35f702bd35be20fc8711
kian0949 <kian0949@edu.campusvejle.dk>
2019-06-25 10:51:17 +0200

    Added functionality to retrieve weather information from the OpenWeatherMapAPI and display it

---
+++ BikeForLife.Services/Entities/Main.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RideService.Services.Entities
+{
+    public class Main
+    {
+        public double Temp { get; set; }
+    }
+}
