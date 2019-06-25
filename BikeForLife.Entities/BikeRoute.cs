ab83cc06a58a7e7525fea5da1d69b74f00b9e366
kian0949 <kian0949@edu.campusvejle.dk>
2019-06-25 11:06:50 +0200

    Added comments to BikeRoute.cs

--- BikeForLife.Entities/BikeRoute.cs
+++ BikeForLife.Entities/BikeRoute.cs
@@ -19,34 +19,34 @@ namespace BikeForLife.Entities
         [Display(Name = "By")]
         public string City { get; set; }
 
-        public string GetIconUrl()
+        public string GetIconUrl() // Retrieves a url for an icon for the City member in the class and returns it as a string
         {
-            OpenWeatherMapAPI openWeatherMapAPI = new OpenWeatherMapAPI();
+            OpenWeatherMapAPI openWeatherMapAPI = new OpenWeatherMapAPI(); // Initializing required variables
             string iconUrl = "";
             try
             {
-                iconUrl = openWeatherMapAPI.GetIconUrl(City);
+                iconUrl = openWeatherMapAPI.GetIconUrl(City); // Tries to run the code
             }
-            catch (WebException)
+            catch (WebException) // If the code can't run and the data isn't retrieved, a WebException is thrown and caught
             {
-                return @"\img\placeholder.png";
+                return @"\img\placeholder.png"; // A URL for a placeholder image is returned instead of the intended URL
             }
-            return iconUrl;
+            return iconUrl; // If the code runs succesfully and the data is retrieved, the URL is returned
         }
 
-        public string GetLocalTemperature()
+        public string GetLocalTemperature() // Retrieves the temperature for the City member in the class and returns it as a string
         {
-            OpenWeatherMapAPI openWeatherMapAPI = new OpenWeatherMapAPI();
+            OpenWeatherMapAPI openWeatherMapAPI = new OpenWeatherMapAPI(); // Intitializing required variables
             double temp = 0.0;
             try
             {
-                temp = openWeatherMapAPI.GetCurrentTemperature(City);
+                temp = openWeatherMapAPI.GetCurrentTemperature(City); // Tries to run the code
             }
-            catch (WebException)
+            catch (WebException) //If the code can't run and the data isn't retrieved, a WebException is thrown and caught
             {
-                return "N/A";
+                return "N/A"; // The output string is set to placeholder text
             }
-            return $"{temp}°C";
+            return $"{temp}°C"; // If the code runs succesfully and the data is retrieved, the text is set to the temperature
         }
     }
 }
